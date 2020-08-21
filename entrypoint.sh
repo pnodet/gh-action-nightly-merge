3002d72cef197a1e340c5fa1fc6b2bcc6a0ffe0f
Paul Nodet <paul.nodet@gmail.com>
2020-08-21 16:00:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,7 +68,7 @@ echo
 set -o xtrace
 
 # Do the merge
-git merge $FF_MODE --no-edit $INPUT_STABLE_BRANCH
+git merge $FF_MODE --allow-unrelated-histories --no-edit $INPUT_STABLE_BRANCH
 
 # Pull lfs if enabled
 if $INPUT_GIT_LFS; then
